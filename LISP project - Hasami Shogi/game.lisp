00e0b6ecad35e049f9430cf0d779f833e58e4322
Unknown <stefanstanko@elfak.rs>
2017-11-01 21:34:51 +0100

    Validacije

--- LISP project - Hasami Shogi/game.lisp
+++ LISP project - Hasami Shogi/game.lisp
@@ -13,12 +13,26 @@
     (showTableRec table nums))
 
 (defun getTableElement (table i j)
-    (getListElement (getListElement table i) j))
+    (cond 
+        ((or (> i (length table)) (< i 0)) table)
+        ((or (> j (length table)) (< j 0)) table)
+        (t (getListElement (getListElement table i) j)))
+)
+    
 
 (defun setTableElement (table el i j)
-    (setListElement table (setListElement (getListElement table i) el j) i))
+    (cond
+        ((or (> i (length table)) (< i 0)) table)
+        ((or (> j (length table)) (< j 0)) table)
+        (t (setListElement table (setListElement (getListElement table i) el j) i)))
+)
 
 ; Playing
 
 (defun nextState (table move fromI fromJ toI toJ)
-    (setTableElement (setTableElement table move toI toJ) '- fromI fromJ))
+    (if (isValidMove table fromJ fromJ toI toJ)
+    (setTableElement (setTableElement table move toI toJ) '- fromI fromJ) table))
+
+(defun isValidMove (table fromI fromJ toI toJ)
+    (if (and (or (= fromI toI) (= fromJ toJ)) (equal '(-) (getTableElement table toI toJ))) t))
+
