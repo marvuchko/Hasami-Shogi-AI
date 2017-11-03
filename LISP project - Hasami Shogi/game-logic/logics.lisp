263452e4e3a84570179179793a2a5aeab9931599
marVuchko <marvuchko@gmail.com>
2017-11-03 13:54:46 +0100

    Gameover detection and sandwich functions declarations

--- LISP project - Hasami Shogi/game-logic/logics.lisp
+++ LISP project - Hasami Shogi/game-logic/logics.lisp
@@ -1,1 +1,17 @@
 (require "./evaluation.lisp")
+
+;;;;;;;;;;; Sandwich detection functions
+
+;detects sandwich vertically
+(defun detect-sandwich-vertically (table)
+    )
+
+;detects sandwich vertically
+(defun detect-sandwich-diagonally (table)
+    )
+
+;;;;;;;;;;; Gameover detection functions
+
+;detects gameover 
+(defun is-game-over(table)
+    )
